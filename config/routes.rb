071ffcd280846e93d04a906b978937b5b9110881
toru tateyama <feet-10@hotmail.co.jp>
2018-05-03 17:39:24 +0900

    parent_categories view add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-   root 'prodacts#index'
+   root 'parent_categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
