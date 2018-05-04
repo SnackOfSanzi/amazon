0fa2faef3422b55fd25b0492a6a7e418b78e9f72
大神琢也 <oogamitakuya@oogamitakuyanoMacBook-Pro.local>
2018-05-04 18:38:40 +0900

    Prodacts->products 名前の修正

--- app/controllers/prodacts_controller.rb
+++
@@ -1,9 +0,0 @@
-class ProdactsController < ApplicationController
-
-  def index
-
-  end
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
    root 'parent_categories#index'
-   resources :prodacts, only: [:show]
+   resources :products, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
