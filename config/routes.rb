e1059259501b16161e4dc4912f573b92b16a3fcf
大神琢也 <oogamitakuya@oogamitakuyanoMacBook-Pro.local>
2018-05-07 19:08:22 +0900

    Scraping

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    root 'parent_categories#index'
 
    resources :parent_categories, only: [:index, :show]
-   resources :products, only: [:show]
+   resources :products, only: [:show, :update]
+   resources :reviews, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
