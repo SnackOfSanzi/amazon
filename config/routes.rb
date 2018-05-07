83ff87ead38fe2f82cc057699ffc2db491ed3a64
toru tateyama <feet-10@hotmail.co.jp>
2018-05-07 18:56:01 +0900

    order and deliveries view add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-   root 'parent_categories#index'
+   root 'products#index'
+
+    resources :orders, only: [:index]
+    resources :deliveries , only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
